130ea3d716c1d88843f081efde660121d6201d31
ruidongl <ruidongl@umich.edu>
2020-03-02 22:28:09 -0500

    Move and Punch

---
+++ Assets/Script/CapsuleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CapsuleController : MonoBehaviour
+{
+    Vector2 movement_input;
+    public float move_speed = 10f;
+    public float rotate_speed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(movement_input.x, 0, movement_input.y) * move_speed * Time.deltaTime;
+        transform.Translate(movement, Space.World);
+    }
+
+    void Rotate()
+    {
+        if (movement_input != Vector2.zero)
+        {
+            Vector3 movement = new Vector3(-movement_input.y, 0.0f, movement_input.x);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.2F);
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        Debug.Log("Moving");
+        movement_input = value.Get<Vector2>();
+    }
+}
---
+++ Assets/Script/PlayerPunch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPunch : MonoBehaviour
+{
+    public GameObject LHand;
+    public GameObject RHand;
+    private bool punch = false;
+    public Vector3 direction;
+
+    public GameObject hand;
+
+    void Start()
+    {
+        direction = new Vector3(1, 0, 0);
+        hand = LHand;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && punch == false)
+        {
+            punch = true;
+            StartCoroutine(Punch());
+            
+        }    
+    }
+
+    IEnumerator Punch()
+    {
+        Vector3 start = hand.transform.position;
+        Vector3 end = hand.transform.position + direction * 1.5f;
+
+        float timeElapsed = 0f;
+        float totalLerpTime = 0.2f;
+
+        while(timeElapsed < totalLerpTime)
+        {
+            hand.transform.position = Vector3.Lerp(start, end, (timeElapsed/totalLerpTime));
+            timeElapsed += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+              timeElapsed = 0f;
+              totalLerpTime = 0.05f;
+
+        while(timeElapsed < totalLerpTime)
+        {
+            hand.transform.position = Vector3.Lerp(end, start, (timeElapsed/totalLerpTime));
+            timeElapsed += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        hand.transform.position = start;
+
+        Debug.Log("enable");
+        if (hand != LHand) hand = LHand;
+        else hand = RHand;
+        punch = false;
+    }
+}
